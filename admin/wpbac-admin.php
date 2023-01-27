e8aa5b62506402cc78d18bce2a830f2f6775751a
rayhanrahman25 <rayhanrahman880@gmail.com>
2023-01-28 00:04:24 +0600

    update: admin menu created

--- admin/wpbac-admin.php
+++ admin/wpbac-admin.php
@@ -1,2 +1,49 @@
 <?php
+
+if ( !defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+/**
+ *	Admin Main
+ */
+
 require_once WPBAC_PATH . 'admin/view/'. WPBAC_FILE_PRFX .'thank-you.php';
+
+class Wpbac_admin{
+    private $wpbac_version;
+
+    public function __construct( $version )
+    {
+       $this->wpbac_version = $version;
+    }
+
+    public function wpbac_admin_menu() {
+        add_menu_page(
+            __( 'Book A Car', WPBAC_TXT_DOMAIN ),
+            __( 'Book A Car', WPBAC_TXT_DOMAIN ),
+            'manage_options',
+            'wpbac-admin-settings',
+            array( $this, WPBAC_PRFX . 'key_settings' ),
+            'dashicons-car',
+            100
+        );
+        add_submenu_page(
+            'wpbac-admin-settings',
+            __( 'Key Settings',WPBAC_TXT_DOMAIN ),
+            __( 'Key Settings',WPBAC_TXT_DOMAIN ),
+            'manage_options',
+            'wpsd-key-settings',
+            array( $this, WPBAC_PRFX . 'key_settings' )
+        );
+        add_submenu_page(
+            'wpbac-admin-settings',
+            __( 'General Settings',WPBAC_TXT_DOMAIN ),
+            __( 'General Settings',WPBAC_TXT_DOMAIN ),
+            'manage_options',
+            'wpsd-general-settings',
+            array( $this, WPBAC_PRFX . 'general_settings' )
+        );
+        
+    }
+}
--- includes/wpbac-main.php
+++ includes/wpbac-main.php
@@ -1,3 +1,33 @@
 <?php
 
+if ( !defined( 'ABSPATH' ) ) {
+    exit;
+}
+
 require_once WPBAC_PATH . 'admin/'. WPBAC_FILE_PRFX .'admin.php';
+
+/**
+ * WPBAC Main Class: Plugin
+ */
+
+ class Wpbac_Main_Class{
+
+    protected $wpbac_version;
+
+    function __construct()
+    {
+        $this->wpbac_version = WPBAC_VERSION;
+        $this->wpbac_trigger_admin_hooks();
+        //$this->wpsd_trigger_front_hooks();
+         //$this->wpsd_load_dependencies();
+    }
+
+    private function wpbac_trigger_admin_hooks(){ 
+       $wpbac_admin = new wpbac_admin($this->wpbac_version);
+       add_action( 'admin_menu', array( $wpbac_admin,  WPBAC_PRFX . 'admin_menu' ) );
+    }
+
+ }
+
+ new wpbac_Main_Class();
+
